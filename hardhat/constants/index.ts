436721c1b9ed1a32c6e846cbd7bbc394aa278434
Rajiv Ramakrishnan <rajiv270992@gmail.com>
2022-12-22 16:58:28 +0530

    feat: first commit

---
+++ hardhat/constants/index.ts
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+export const WHITELIST_CONTRACT_ADDRESS =
+  '0xbCC0F2A717A94a2dBC5176CaEF3c1C5D8C780D8d';
+
+// URL to extract Metadata for a Crypto Dev NFT
+export const METADATA_URL = 'https://nft-collection-tau-red.vercel.app/api/';
---
+++ hardhat/scripts/deploy.ts
@@ -0,0 +1,33 @@
+import { ethers } from 'hardhat';
+import { METADATA_URL, WHITELIST_CONTRACT_ADDRESS } from '../constants';
+
+async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+  */
+  const cryptoDevsContract = await ethers.getContractFactory('CryptoDevs');
+
+  // deploy the contract
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  // print the address of the deployed contract
+  console.log(
+    'Crypto Devs Contract Address:',
+    deployedCryptoDevsContract.address
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
